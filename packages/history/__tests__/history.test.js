aef5c9d1cc1082edffc07efe0737fb9077ccc53a
kern.yoo <kern86@wemakeprice.com>
2019-08-10 21:28:23 +0900

    feat(QU-7): History module added
    1. Added history package

---
+++ packages/history/__tests__/history.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const history = require('..');
+
+describe('@quetils/history', () => {
+    it('needs tests');
+});
---
+++ packages/history/lib/history.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = history;
+
+function history() {
+    // TODO
+}
