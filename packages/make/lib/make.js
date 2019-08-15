729045e45484c8b9bf735932b7159ff50d005f5d
kern.yoo <kern86@wemakeprice.com>
2019-08-16 01:55:00 +0900

    feat(QU-7): Add make object function
    1. Add `makeObject` function
    2. Update test case

--- packages/make/lib/make.js
+++ packages/make/lib/make.js
@@ -6,19 +6,31 @@ const set = require('@quetils/set');
 module.exports = make;
 
 /**
+ * Make query string object
+ * 
+ * @param {Object} [obj = {}] Object to make
+ * @return {Object} Object after make
+ */
+function makeObject(obj = {}, url = '') {
+  const target = Object.assign({}, obj);
+
+  const arr = split(url);
+  const splitFunc = set(target);
+  arr.forEach(splitFunc);
+  return target;
+}
+
+
+/**
  * url query string make object
  *
  * @param {string} url url string
  * @return {Object} make query object
  */
 function make(url) {
-  const queries = {};
-
-  if (url) {
-    const arr = split(url);
-    const splitFunc = set(queries);
-    arr.forEach(splitFunc);
-  }
+  let queries = {};
+  
+  if (url) { queries = makeObject(queries, url); }
 
   return queries;
 }
